317473a4aea8dfb1d7b7a0e1b1b187e3c3b350a9
hariv1 <hariv@rose-hulman.edu>
2018-01-31 16:17:17 -0500

    I changed m1 to hariv1

--- sandbox/hariv1/motors/m2_drive_encoders.py
+++ sandbox/hariv1/motors/m2_drive_encoders.py
@@ -10,6 +10,48 @@ Authors: David Fisher and Vikram Hari.
 # TODO: 2. Copy the contents of your m1_drive_timed.py and paste that text into this file below these comments.
 #   If your program says and prints anything at the start change it to print and say "Drive using encoders"
 
+import ev3dev.ev3 as ev3
+import time
+
+
+def main():
+    print("--------------------------------------------")
+    print("  Drive using encoders")
+    print("--------------------------------------------")
+    ev3.Sound.speak("Drive distance").wait()
+
+    # Connect two large motors on output ports B and C
+    left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+    right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+
+    # Check that the motors are actually connected
+    assert left_motor.connected
+    assert right_motor.connected
+
+    time_s = 1  # Any value other than 0.
+    while time_s != 0:
+        speed = int(input("Enter a speed (0 to 900 dps): "))
+        if speed == 0:
+            break
+        distance = int(input("Distance to travel (inches): "))
+        if distance == 0:
+            break
+        left_motor.run_forever(speed_sp=speed)
+        right_motor.run_forever(speed_sp=speed)
+        time.sleep(distance/(0.011518413*speed))
+        left_motor.stop()
+        right_motor.stop()
+        ev3.Sound.beep().wait()
+
+    print("Goodbye!")
+    ev3.Sound.speak("Goodbye").wait()
+
+
+# ----------------------------------------------------------------------
+# Calls  main  to start the ball rolling.
+# ----------------------------------------------------------------------
+main()
+
 # TODO: 3. Add a beep after the drive motors stop (see code below).  Test your code to hear the beep AFTER movement.
 #   ev3.Sound.beep().wait()
 
