7513988333af67a9e09d8cf20608a3ad51e143e3
tuj <tuj@rose-hulman.edu>
2018-02-04 22:51:54 -0500

    update

--- libs/robot_controller.py
+++ libs/robot_controller.py
@@ -93,6 +93,9 @@ class Snatch3r(object):
 
     def arm_down(self):
         """(Moves the Snatch3r arm to the down position.)"""
+        self.arm_motor.run_to_abs_pos(position_sp=0, speed_sp=900)
+        self.arm_motor.wait_while(self.arm_motor.STATE_RUNNING)
+        ev3.Sound.beep().wait()
 
 
 
