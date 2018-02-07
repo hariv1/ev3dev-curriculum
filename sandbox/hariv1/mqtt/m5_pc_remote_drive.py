0cae48f2831231d45436c64e0d5980e4de92dcc5
hariv1 <hariv@rose-hulman.edu>
2018-02-06 23:01:20 -0500

    I changed m1 to hariv1

--- sandbox/hariv1/mqtt/m5_pc_remote_drive.py
+++ sandbox/hariv1/mqtt/m5_pc_remote_drive.py
@@ -143,12 +143,13 @@ def callback_forward(mqtt_client, left_speed_entry, right_speed_entry):
 def callback_left(mqtt_client, left_speed_entry, right_speed_entry):
     print("Left")
     mqtt_client.send_message("red_up",[int(left_speed_entry.get())])
+    mqtt_client.send_message("blue_down", [int(right_speed_entry.get())])
 
 
 def callback_right(mqtt_client, left_speed_entry, right_speed_entry):
     print("Right")
     mqtt_client.send_message("blue_up",[int(right_speed_entry.get())])
-
+    mqtt_client.send_message("red_down", [int(left_speed_entry.get())])
 
 def callback_back(mqtt_client, left_speed_entry, right_speed_entry):
     print("Back")
