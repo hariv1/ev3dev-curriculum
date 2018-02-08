6117fa026a5ce400439a91a2c8daa845b4d69a9c
hariv1 <hariv@rose-hulman.edu>
2018-02-07 23:11:28 -0500

    I changed m1 to hariv1

--- sandbox/hariv1/analog_sensors/color_sensor/m1_drive_to_the_color.py
+++ sandbox/hariv1/analog_sensors/color_sensor/m1_drive_to_the_color.py
@@ -87,24 +87,35 @@ def drive_to_color(button_state, robot, color_to_seek):
       :type robot: robo.Snatch3r
       :type color_to_seek: int
     """
+
     if button_state:
         ev3.Sound.speak("Seeking " + COLOR_NAMES[color_to_seek]).wait()
 
-        # TODO: 3. Implement the task as stated in this module's initial comment block
+        # DONE: 3. Implement the task as stated in this module's initial
+        # comment block
         # It is recommended that you add to your Snatch3r class's constructor the color_sensor, as shown
         #   self.color_sensor = ev3.ColorSensor()
         #   assert self.color_sensor
         # Then here you can use a command like robot.color_sensor.color to check the value
 
-        robot.forward(500, 500)
+        while True:
 
-        if robot.color_sensor.color == color_to_seek:
-            robot.stop()
-            ev3.Sound.speak("Found " + COLOR_NAMES[color_to_seek]).wait()
+            if robot.color_sensor.color == color_to_seek:
+                robot.stop()
+                ev3.Sound.speak("Found " + COLOR_NAMES[color_to_seek]).wait()
+                break
+
+            robot.forward(500, 500)
+
+
+    else:
+        robot.stop()
 
 
 
-        # TODO: 4. Call over a TA or instructor to sign your team's checkoff sheet.
+
+        # DONE: 4. Call over a TA or instructor to sign your team's checkoff
+        # sheet.
         #
         # Observations you should make, the instance variable robot.color_sensor.color is always updating
         # to the color seen and that value is given to you as an int.
