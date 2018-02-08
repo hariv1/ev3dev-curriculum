d505dc420206bc33b6724bd00443bd84a37313cc
hariv1 <hariv@rose-hulman.edu>
2018-02-07 22:47:24 -0500

    I changed m1 to hariv1

--- sandbox/hariv1/analog_sensors/color_sensor/m1_drive_to_the_color.py
+++ sandbox/hariv1/analog_sensors/color_sensor/m1_drive_to_the_color.py
@@ -13,8 +13,8 @@ If the user presses the Down  button, the robot drives until the robot gets to B
 If the user presses the Left  button, the robot drives until the robot gets to Black.
 If the user presses the Right button, the robot drives until the robot gets to White.
 
-Authors: David Fisher and Justin.
-"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
+Authors: David Fisher and PUT_YOUR_NAME_HERE.
+"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import ev3dev.ev3 as ev3
 import time
@@ -40,8 +40,6 @@ class DataContainer(object):
 
     def __init__(self):
         self.running = True
-        self.color_sensor = ev3.ColorSensor()
-        assert self.color_sensor
 
 
 def main():
@@ -60,12 +58,11 @@ def main():
 
     # For our standard shutdown button.
     btn = ev3.Button()
-    # done: 2. Uncomment the lines below to setup event handlers for these buttons.
-
-    btn.on_up = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_RED)
-    btn.on_down = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_BLUE)
-    btn.on_left = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_BLACK)
-    btn.on_right = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_WHITE)
+    # TODO: 2. Uncomment the lines below to setup event handlers for these buttons.
+    # btn.on_up = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_RED)
+    # btn.on_down = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_BLUE)
+    # btn.on_left = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_BLACK)
+    # btn.on_right = lambda state: drive_to_color(state, robot, ev3.ColorSensor.COLOR_WHITE)
     btn.on_backspace = lambda state: handle_shutdown(state, dc)
 
     while dc.running:
@@ -89,13 +86,10 @@ def drive_to_color(button_state, robot, color_to_seek):
       :type robot: robo.Snatch3r
       :type color_to_seek: int
     """
-
-    current_color = robot.color_sensor
     if button_state:
         ev3.Sound.speak("Seeking " + COLOR_NAMES[color_to_seek]).wait()
 
-        # done: 3. Implement the task as stated in this module's initial
-        # comment block
+        # TODO: 3. Implement the task as stated in this module's initial comment block
         # It is recommended that you add to your Snatch3r class's constructor the color_sensor, as shown
         #   self.color_sensor = ev3.ColorSensor()
         #   assert self.color_sensor
