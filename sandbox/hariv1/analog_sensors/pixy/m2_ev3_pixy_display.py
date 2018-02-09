7726024e1da2ecddc60ba53a7d7fd0093e67b8a4
hariv1 <hariv@rose-hulman.edu>
2018-02-09 01:21:16 -0500

    I changed m1 to hariv1

--- sandbox/hariv1/analog_sensors/pixy/m2_ev3_pixy_display.py
+++ sandbox/hariv1/analog_sensors/pixy/m2_ev3_pixy_display.py
@@ -21,23 +21,40 @@ def main():
     ev3.Sound.speak("Pixy display").wait()
     print("Press the touch sensor to exit this program.")
 
-    # TODO: 2. Create an MqttClient (no delegate needed since EV3 will only send data, so an empty constructor is fine)
+    # DONE: 2. Create an MqttClient (no delegate needed since EV3 will only
+    # send data, so an empty constructor is fine)
     # Then connect to the pc using the connect_to_pc method.
 
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_pc()
 
     robot = robo.Snatch3r()
     robot.pixy.mode = "SIG1"
 
     while not robot.touch_sensor.is_pressed:
 
-        # TODO: 3. Read the Pixy values for x, y, width, and height
+        # DONE: 3. Read the Pixy values for x, y, width, and height
         # Print the values (much like the print_pixy_readings example)
 
-        # TODO: 4. Send the Pixy values to the PC by calling the on_rectangle_update method
+        x = robot.pixy.value(1)
+        print(x)
+
+        y = robot.pixy.value(2)
+        print(y)
+
+        width = robot.pixy.value(3)
+        print(width)
+
+        height = robot.pixy.value(4)
+        print(height)
+
+
+        # DONE: 4. Send the Pixy values to the PC by calling the
+        # on_rectangle_update method
         # If you open m2_pc_pixy_display you can see the parameters for that method [x, y, width, height]
 
 
-
+        mqtt_client.send_message("on_rectangle_update", [x, y, width, height])
 
         time.sleep(0.25)
 
@@ -45,7 +62,7 @@ def main():
     ev3.Sound.speak("Goodbye").wait()
     mqtt_client.close()
 
-# TODO: 5. Call over a TA or instructor to sign your team's checkoff sheet.
+# DONE: 5. Call over a TA or instructor to sign your team's checkoff sheet.
 #
 # Observations you should make, if the EV3 has data the PC can know that data too using MQTT.
 
