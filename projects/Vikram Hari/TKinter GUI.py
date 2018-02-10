e435b03d78ebcbe546ed882a864becff44644dfa
hariv1 <hariv@rose-hulman.edu>
2018-02-10 16:42:16 -0500

    I changed m1 to hariv1

--- projects/Vikram Hari/TKinter GUI.py
+++ projects/Vikram Hari/TKinter GUI.py
@@ -15,29 +15,37 @@ def main():
     main_frame = ttk.Frame(root, padding=200)
     main_frame.grid()
 
-    game_label = ttk.Label(main_frame, text="Mars Rover", font=("Helvetica", 16))
+    game_label = tkinter.Label(main_frame, text="Mars Rover", font=(
+        "Helvetica", 24), fg='blue')
     game_label.grid(row=0, column=0)
 
     start_game_button = ttk.Button(main_frame, text="Start Game")
-    start_game_button.grid(row=1, column=0)
-    start_game_button['command'] = lambda: main_interface()
+    start_game_button.grid(row=3, column=0)
+    start_game_button['command'] = lambda: main_interface(root)
 
     exit_game_button = ttk.Button(main_frame, text="End Game")
-    exit_game_button.grid(row=2, column=0)
+    exit_game_button.grid(row=4, column=0)
     exit_game_button['command'] = lambda: quit(mqtt_client, True)
 
     root.mainloop()
 
 
-def main_interface():
+def main_interface(root):
 
-    root = tkinter.Tk()
-    root.title("Missions")
+    root.withdraw()
 
-    main_frame = ttk.Frame(root, padding=200)
-    main_frame.grid()
+    new_game_window = tkinter.Toplevel()
+    new_game_window.title("Missions")
 
-    root.mainloop()
+    frame_2 = ttk.Frame(new_game_window,padding=200)
+    frame_2.grid()
+
+    mission_1_button = ttk.Button(frame_2, text="Mission 1:\nA New Era")
+    mission_1_button.grid(row=0, column=0)
+    mission_1_button['command'] = lambda:
+
+
+
 
 def quit(mqtt_client, shutdown_ev3):
     if shutdown_ev3:
