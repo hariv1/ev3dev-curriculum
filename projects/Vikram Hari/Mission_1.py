64a975b11d7b8d1c6401c56b450d070402dc0990
hariv1 <hariv@rose-hulman.edu>
2018-02-10 17:41:48 -0500

    I changed m1 to hariv1

--- projects/Vikram Hari/Mission_1.py
+++ projects/Vikram Hari/Mission_1.py
@@ -1,2 +1,25 @@
+
+import traceback
+
+import ev3dev.ev3 as ev3
+import time
+import math
+
+import robot_controller as robo
+
+
 def main():
-    print("hjadshgj")
+    print("\nBeacon the controller\n")
+    ev3.Sound.speak("Find the controller or we can't get back home").wait()
+
+    robot = robo.Snatch3r()
+
+    while True:
+        find_the_beacon = robot.seek_beacon()
+        if find_the_beacon:
+            ev3.Sound.speak("I got the beacon")
+            robot.arm_up()
+            time.sleep(1)
+            robot.arm_down()
+
+main()
