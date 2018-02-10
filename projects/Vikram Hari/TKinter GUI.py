99141b7c2d510229f3c4422ab9e02b5a927e6379
hariv1 <hariv@rose-hulman.edu>
2018-02-10 17:36:47 -0500

    I changed m1 to hariv1

--- projects/Vikram Hari/TKinter GUI.py
+++ projects/Vikram Hari/TKinter GUI.py
@@ -3,6 +3,8 @@ import tkinter
 from tkinter import ttk
 import mqtt_remote_method_calls as com
 import Mission_1
+import Mission_2
+import Mission_3
 
 def main():
 
@@ -12,10 +14,10 @@ def main():
     root = tkinter.Tk()
     root.title("Mars Rover")
 
-    main_frame = ttk.Frame(root, padding=200)
+    main_frame = ttk.Frame(root, padding=100)
     main_frame.grid()
 
-    game_label = tkinter.Label(main_frame, text="Mars Rover", font=(
+    game_label = tkinter.Label(main_frame, text="Mars Rover\n\n\n", font=(
         "Helvetica", 24), fg='blue')
     game_label.grid(row=0, column=0)
 
@@ -34,27 +36,37 @@ def main_interface(root):
 
     root.withdraw()
 
+    mqtt_client = com.MqttClient()
+    mqtt_client.connect_to_ev3()
+
     new_game_window = tkinter.Toplevel()
     new_game_window.title("Missions")
 
-    frame_2 = ttk.Frame(new_game_window,padding=200)
+    frame_2 = ttk.Frame(new_game_window,padding=80)
     frame_2.grid()
 
-    mission_1_button = tkinter.Button(frame_2, text="Mission 1\nA New Era")
-    mission_1_button.grid(row=0, column=0)
-    if __name__ == '__main__':
-        mission_1_button['command'] = lambda: Mission_1.main()
+    missions_label = tkinter.Label(frame_2, text="Missions\n", font=(
+        "Helvetica", 24), fg='red')
+    missions_label.grid(row=0, column=0)
 
-    mission_2_button = tkinter.Button(frame_2, text="Mission 2\n")
-    mission_2_button.grid(row=1, column=0)
+    mission_1_button = tkinter.Button(frame_2, text="Mission 1\nFind "
+                                                    "Controller")
+    mission_1_button.grid(row=1, column=0, pady= 20)
+    mission_1_button['command'] = lambda: Mission_1.main()
+
+    mission_2_button = tkinter.Button(frame_2, text="Mission 2\nDrive back "
+                                                    "via Satellite")
+    mission_2_button.grid(row=2, column=0, pady= 20)
     mission_2_button['command'] = lambda: Mission_2.main()
 
-    mission_3_button = tkinter.Button(frame_2, text="Mission 3\n")
-    mission_3_button.grid(row=1, column=0)
+    mission_3_button = tkinter.Button(frame_2, text="Mission 3\nFood and "
+                                                    "Water")
+    mission_3_button.grid(row=3, column=0, pady= 20)
     mission_3_button['command'] = lambda: Mission_3.main()
 
-
-
+    exit_game_button = ttk.Button(frame_2, text="End Game")
+    exit_game_button.grid(row=4, column=0)
+    exit_game_button['command'] = lambda: quit(mqtt_client, True)
 
 
 
@@ -64,16 +76,6 @@ def quit(mqtt_client, shutdown_ev3):
         mqtt_client.send_message("shutdown")
     mqtt_client.close()
     exit()
-
-
-
-
-
-
-
-
-
-
 
 
 
