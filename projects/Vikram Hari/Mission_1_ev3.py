d4a0c5f10dec797425afe01da0d7b221cacd4903
hariv1 <hariv@rose-hulman.edu>
2018-02-14 17:11:03 -0500

    Robot Controller Update

--- projects/Vikram Hari/Mission_1_ev3.py
+++ projects/Vikram Hari/Mission_1_ev3.py
@@ -11,8 +11,8 @@ def main():
     found_the_beacon = robot.seek_beacon()
 
     if found_the_beacon:
-        #ev3.Sound.speak("Oh no, it seems like my autonomous driving "
-        #                "features do not work")
+        ev3.Sound.speak("Oh no, it seems like my autonomous driving "
+                        "features do not work")
         ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
         ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.RED)
 
