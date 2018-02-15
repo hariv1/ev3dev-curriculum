7ae1969286e5f474aa84c95cf9cac7eb1ea9eda8
hariv1 <hariv@rose-hulman.edu>
2018-02-15 00:38:22 -0500

    Robot Controller Update

--- projects/Vikram Hari/TKinter GUI.py
+++ projects/Vikram Hari/TKinter GUI.py
@@ -56,13 +56,21 @@ def main_interface(root):
     mission_2_button.grid(row=2, column=0, pady=20)
     mission_2_button['command'] = lambda: callback_mission_2(new_game_window)
 
-    mission_3_button = tkinter.Button(frame_2, text="Mission 3\nFood and "
+    mission_3_button = tkinter.Button(frame_2, text="Mission 3\n"
                                                     "Water")
     mission_3_button.grid(row=3, column=0, pady=20)
     mission_3_button['command'] = lambda: callback_mission_3(new_game_window)
+
+    mission_4_button = tkinter.Button(frame_2, text="Mission 4\n"
+                                                    "Food")
+    mission_4_button.grid(row=4, column=0, pady=20)
+    mission_4_button['command'] = lambda: callback_mission_4(new_game_window)
+
     exit_game_button = ttk.Button(frame_2, text="End Game")
-    exit_game_button.grid(row=4, column=0)
+    exit_game_button.grid(row=5, column=0)
     exit_game_button['command'] = lambda: quit(mqtt_client, True)
+
+
 
 
 def quit(mqtt_client, shutdown_ev3):
@@ -124,14 +132,29 @@ def callback_mission_3(m2_window):
     m2_window.destroy()
 
     m3_window = tkinter.Toplevel()
-    m3_window.title("Mission 3: Use Beacon to search for food")
+    m3_window.title("Mission 3: Use Beacon to search for Water")
 
     frame_m3 = ttk.Frame(m3_window, padding=80)
     frame_m3.grid()
 
     instructions_label = tkinter.Label(frame_m3, text="Run Mission_3_ev3 "
                                                       "on SSH Terminal\n\nUse "
-                                                      "Beacon to pick up and bring back food/water\n")
+                                                      "Beacon to pick up and bring back water\n")
+    instructions_label.grid(row=0, column=0)
+
+
+def callback_mission_4(m3_window):
+    m3_window.destroy()
+
+    m4_window = tkinter.Toplevel()
+    m4_window.title("Mission 4: Use Beacon to search for food")
+
+    frame_m3 = ttk.Frame(m4_window, padding=80)
+    frame_m3.grid()
+
+    instructions_label = tkinter.Label(frame_m3, text="Run Mission_4_ev3 "
+                                                      "on SSH Terminal\n\nUse "
+                                                      "Beacon to pick up and bring back food\n")
     instructions_label.grid(row=0, column=0)
 
 
